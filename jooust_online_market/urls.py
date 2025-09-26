ae381b78c7b9d3240e74f0b14a07dbb14232a8d1
Victor Owino <vo4685336@gmail.com>
2025-09-26 21:48:32 +0300

    include products URLs

--- jooust_online_market/urls.py
+++ jooust_online_market/urls.py
@@ -8,6 +8,7 @@ from django.urls.conf import re_path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("products.urls")),
     re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
 ]
 if settings.DEBUG:
