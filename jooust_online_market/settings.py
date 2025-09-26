2175ddbf4d42d89208abdeb9c0bf1732ce62e222
Victor Owino <vo4685336@gmail.com>
2025-09-27 00:07:14 +0300

    change other apps to your apps

--- jooust_online_market/settings.py
+++ jooust_online_market/settings.py
@@ -19,7 +19,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # Other Apps
+    # Your Apps
     "users",
     "products",
 ]
