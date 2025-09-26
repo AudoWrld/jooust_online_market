b611ba8d94eb68f32ab38276cbbac09b3a1bd3d8
Victor Owino <vo4685336@gmail.com>
2025-09-26 22:30:45 +0300

    feat(products): add Product and ProductImage admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,21 @@
+# Products admin
 from django.contrib import admin
+from .models import Product, ProductImage
 
-# Register your models here.
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name", "user", "selling_price", "discount_price", "quantity"]
+    list_filter = ["user"]
+    search_fields = ["name", "description", "user__email"]
+    inlines = [ProductImageInline]
+
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ["product", "image"]
