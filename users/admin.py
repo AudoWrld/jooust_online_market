feeabb0f5a06b5d8683168645e1df405e787390f
Victor Owino <vo4685336@gmail.com>
2025-09-26 22:29:37 +0300

    feat(users): add User admin support

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,66 @@
+# User Admin
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ["email"]
+    list_display = [
+        "email",
+        "first_name",
+        "second_name",
+        "email_verified",
+        "is_staff",
+        "is_active",
+    ]
+    list_filter = ["is_staff", "is_superuser", "is_active", "email_verified"]
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            _("Personal info"),
+            {
+                "fields": (
+                    "first_name",
+                    "second_name",
+                    "mpesa_phone_number",
+                    "email_verified",
+                )
+            },
+        ),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login",)}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "first_name",
+                    "second_name",
+                    "password1",
+                    "password2",
+                ),
+            },
+        ),
+    )
+
+    search_fields = ["email", "first_name", "second_name"]
